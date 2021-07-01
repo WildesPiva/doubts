17cdc52171a1d12897879048277ba73ad44bce66
Wildes Piva <wildes.piva@nappsolutions.com>
2021-07-01 17:53:29 -0300

    Add tooltip and reaction on click copyClipboard

--- src/components/RoomCode/index.tsx
+++ src/components/RoomCode/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import ReactTooltip from 'react-tooltip'
 import { ButtonHTMLAttributes } from 'react'
 import copyImage from '../../assets/images/copy.svg'
 import styles from './styles.module.scss'
@@ -7,17 +9,21 @@ type RoomCodeProps = ButtonHTMLAttributes<HTMLButtonElement> & {
 }
 
 export function RoomCode({ code, ...props }: RoomCodeProps) {
+    const [tooltip, setTooltop] = useState('Copiar para área de transferência')
 
     function copyRoomCodeToClipboard() {
+        setTooltop('Copiado')
         navigator.clipboard.writeText(code)
+        setTimeout(() => setTooltop('Copiar para área de transferência'), 1000)
     }
 
-    return (
-        <button className={styles.roomCode} onClick={copyRoomCodeToClipboard} {...props}>
+    return (<>
+        <ReactTooltip />
+        <button data-tip={tooltip} className={styles.roomCode} onClick={copyRoomCodeToClipboard} {...props}>
             <div>
                 <img src={copyImage} alt="Copy room code" />
             </div>
             <span> Sala #{code}</span>
         </button>
-    )
+    </>)
 }
